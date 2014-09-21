eaca679763b981cbe5036a36a74bb1a75f3e3d63
Nicolas McCurdy <thenickperson@gmail.com>
2014-09-20 20:34:46 -0400

    Add a simple boilerplate for AngularJS

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var app = angular.module('justify', []);
+
+// The main controller for Justify
+app.controller('JustifyController', function ($scope) {
+  $scope.message = 'Hello, world!';
+});
