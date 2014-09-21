4f689bad38698c64c7bbc5ba051b5dec7cd88b14
Nicolas McCurdy <thenickperson@gmail.com>
2014-09-21 00:29:23 -0400

    Support using a JSON file for configuration

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 var app = angular.module('justify', []);
 
 // The main controller for Justify
-app.controller('JustifyController', function ($scope) {
-  var nouns = ['effort', 'an in depth knowledge of Latin', 'me not sleeping'];
-
-  $scope.generate = function () {
-    $scope.noun = _.sample(nouns);
-  };
-
-  $scope.generate();
+app.controller('JustifyController', function ($scope, $http) {
   $scope.defaultSituation = 'do something useful';
-});
 
+  $http.get('data.json').success(function (data) {
+    $scope.generate = function () {
+      $scope.noun = _.sample(data.nouns);
+    };
+
+    $scope.generate();
+  });
+});
