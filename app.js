a9876bd4eea4937fd5e397a21d2cc3accd47389c
Nicolas McCurdy <thenickperson@gmail.com>
2014-09-21 16:38:37 -0400

    Add a simple history display

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ var app = angular.module('justify', []);
 // The main controller for Justify
 app.controller('JustifyController', function ($scope, $http) {
   $scope.defaultSituation = 'do something useful';
+  $scope.excuseHistory = [];
 
   // Load JSON data
   $http.get('data.json').success(function (data) {
     // Chooses a new random noun
     $scope.generate = function () {
-      $scope.excuse = _.template(_.sample(data.excuses).string)(data);
+      if ($scope.currentExcuse) {
+        $scope.excuseHistory.unshift($scope.currentExcuse);
+      }
+
+      $scope.currentExcuse = _.template(_.sample(data.excuses).string)(data);
     };
 
     $scope.generate();
