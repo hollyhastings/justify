4fd7e2c0bb7358348811e17e9a03d36029fa0311
Nicolas McCurdy <thenickperson@gmail.com>
2014-09-21 00:40:18 -0400

    Add comments

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// The Angular module for all of Justify
 var app = angular.module('justify', []);
 
 // The main controller for Justify
 app.controller('JustifyController', function ($scope, $http) {
   $scope.defaultSituation = 'do something useful';
 
+  // Load JSON data
   $http.get('data.json').success(function (data) {
+    // Chooses a new random noun
     $scope.generate = function () {
       $scope.noun = _.sample(data.nouns);
     };
